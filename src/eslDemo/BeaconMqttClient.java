ba6635bbc4684c85ba694dc6ff4c6ae11b476215
hogen <hogen@kkmcn.com>
2018-12-05 18:34:07 +0800

    bugfix

--- src/eslDemo/BeaconMqttClient.java
+++ src/eslDemo/BeaconMqttClient.java
@@ -4,6 +4,8 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import eslDemo.BeaconMqttPushCallback;
+
 import eslDemo.MqttConnNotify.ConnectionNotify;
 
 public class BeaconMqttClient 
@@ -32,6 +34,42 @@ public class BeaconMqttClient
     	mPublishTopic = strPublishTopic;
     	mUserName = usrPwd;
     	mPassWord = password;
+    	
+    	mHostAddr = strHost;
+    	mPublishTopic = strPublishTopic;
+    	mUserName = usrPwd;
+    	mPassWord = password;
+    	
+        try {  
+            // host为主机名，clientid即连接MQTT的客户端ID，一般以唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存  
+        	clientid = clientid + Math.random();
+        	mClient = new MqttClient(mHostAddr, clientid, new MemoryPersistence());  
+             
+            // MQTT的连接设置  
+            options = new MqttConnectOptions();  
+            
+            // 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接  
+            options.setCleanSession(true);  
+            
+            // 设置连接的用户名  
+            options.setUserName(mUserName);  
+            
+            // 设置连接的密码  
+            options.setPassword(mPassWord.toCharArray()); 
+            
+            // 设置超时时间 单位为秒  
+            options.setConnectionTimeout(30);  
+            
+            // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制  
+            options.setKeepAliveInterval(20);  
+            
+            // 设置回调  
+            mBeaconCallback = new BeaconMqttPushCallback(this, mQttNotify);
+            mClient.setCallback(mBeaconCallback); 
+            
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        } 
     }
     
     public String getHostAddr()
@@ -69,40 +107,7 @@ public class BeaconMqttClient
   
     private void initParamaters() 
     {  
-        try {  
-            // host为主机名，clientid即连接MQTT的客户端ID，一般以唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存  
-        	clientid = clientid + Math.random();
-        	mClient = new MqttClient(mHostAddr, clientid, new MemoryPersistence());  
-            
-            // MQTT的连接设置  
-            options = new MqttConnectOptions();  
-            
-            // 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接  
-            options.setCleanSession(true);  
-            
-            // 设置连接的用户名  
-            options.setUserName(mUserName);  
-            
-            // 设置连接的密码  
-            options.setPassword(mPassWord.toCharArray()); 
-            
-            // 设置超时时间 单位为秒  
-            options.setConnectionTimeout(30);  
-            
-            // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制  
-            options.setKeepAliveInterval(20);  
-            
-            // 设置回调  
-            mBeaconCallback = new BeaconMqttPushCallback(this, mQttNotify);
-            mClient.setCallback(mBeaconCallback);  
-            //MqttTopic topic = client.getTopic(SUB_TOPIC);  
-            
-            //setWill方法，如果项目中需要知道客户端是否掉线可以调用该方法。设置最终端口的通知消息    
-            //options.setWill(topic, "close".getBytes(), 2, true);  
-            
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
+        
     }   
     
     public synchronized void connect()
